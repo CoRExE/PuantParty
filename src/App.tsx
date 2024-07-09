5f3b5d9f34601501ba1a080bd19ad4f83a5c3a34
ceestos <jules.langagne@gmail.com>
2024-07-09 07:11:15 +0200

    Some Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CountdownTimer: React.FC = () => {
+    const calculateTimeLeft = () => {
+        const difference = +new Date('2024-08-01T00:00:00') - +new Date();
+        let timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-export default App;
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    return (
+        <div className={"card"}>
+            <h1>Qui sera le plus Grand Puant ?</h1>
+            <p>{timeLeft.days} Jours {timeLeft.hours} Heures {timeLeft.minutes} Minutes {timeLeft.seconds} Secondes</p>
+        </div>
+    );
+};
+
+export default CountdownTimer;
